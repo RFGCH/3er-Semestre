9968406c6c176b5d7468b0a5b94a968ceae557dd
RFGCH <54367628+RFGCH@users.noreply.github.com>
2020-04-23 16:50:17 -0500

    Comprobando con el modulo de la practica
    Se agrego:
    
    - El metodo de obtencion del modulo de la practica, pag9.

--- Division_entera/main.cpp
+++ Division_entera/main.cpp
@@ -40,13 +40,23 @@ int fun_mod(int a,int b){
     if(i)return b-a;
     return a-b+aux;
 }
-
+int cquo(int a,int b){
+    if(b==0)return a;
+    int q = int(a/b);
+    if(b<0)q=q+1;
+    return q;
+}
+int crem(int a,int b){
+    return a-b*cquo(a,b);
+}
 int fun_mcd(int a, int b){
     if(b==0)return a;
     int r = fun_mod(a,b);
     fun_mcd(b,r);
 }
-
+int fun_mod2(int a, int b){
+    return a%b;
+}
 bool fun_divisivilidad(int a, int b){
     if(fun_mod(b,a)==0)
         return true;
@@ -62,25 +72,39 @@ int main()
     i=a.get_div();
     j=a.get_res();
     cout << i << " " << j << endl;
-    cout << fun_mcd(4864,3458) << endl;
+  //  cout << fun_mcd(4864,3458) << endl;
 
-/*
+
     unsigned t0, t1;
 
     t0=clock();
-    for(int i=0;i<20000000;i++)
-        */
-    cout << fun_mcd(260,152);
-    /*
+    cout <<  fun_mod(260,152);
+    for(int i=0;i<20000000;i++){
+        fun_mod(260,152);//Aumenta en un 10-8% la velocidad sin la evaluacion de -a;
+    }
+
     t1=clock();
 
     double time = (double(t1-t0)/CLOCKS_PER_SEC);
     cout << "Execution Time: " << time << endl;
 
     t0=clock();
-    for(int i=0;i<20000000;i++)
+    cout << 260-152*int(260/152);
+    for(int i=0;i<200000000;i++){
+        260-152*int(260/152);//Cream sin llamar a la funcion;
+    }
 
-    8%3;
+    t1 = clock();
+
+    time = (double(t1-t0)/CLOCKS_PER_SEC);
+    cout << "Execution Time: " << time << endl;
+
+    t0=clock();
+    cout << 260%152;
+    for(int i=0;i<200000000;i++){
+        260%152;//Modulo directo
+    }
+
     t1 = clock();
 
     time = (double(t1-t0)/CLOCKS_PER_SEC);
@@ -90,7 +114,6 @@ int main()
 
 
 
-*/
 
     return 0;
 }
